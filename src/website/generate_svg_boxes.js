83c90509960ed4d3e1b65fe369784b23523114ef
Unknown <alex.mailliu21@gmail.com>
2018-04-27 11:26:41 +0200

    comment, clear updtate

--- src/website/generate_svg_boxes.js
+++ src/website/generate_svg_boxes.js
@@ -75,8 +75,10 @@ var svg_builder = {
 	 *	then we use the magic function click that simulate a human click on this a tag, which open the download yes/no window.
 	 */
 	generate_svg_file: function () {
-		// resetting the viewbox
-		svg_builder.set_viewbox();
+		svg_builder.define_attributes_box(BOX_SCHEME_WIDTH, BOX_SCHEME_LENGTH);
+		// hidding the wooden plate
+		var layer = document.getElementById("svgLayer2"); 
+		layer.setAttribute("opacity", "0"); 
 		// Use XMLSerializer to convert the DOM to a string
 		var s = new XMLSerializer();
 		var d = document.getElementById("svg");
@@ -89,6 +91,10 @@ var svg_builder = {
 		document.getElementById("filesvg").setAttribute("download", "thismustbethebest.svg");
 		// our a tag is hidden, so we use the click function as we would click on it usualy
 		document.getElementById("filesvg").click();
+		// show the wooden plate
+		layer.setAttribute("opacity", "1"); 
+		// on retire la viewBox pour que notre affichage sur le site reste visible avec des proportions correctes
+		d.removeAttribute("transform");
 	},
 
 	/** 
@@ -110,13 +116,13 @@ var svg_builder = {
 		svg_builder.clear_svg("svgLayer2");
 		// to draw (in a second g layout) the wooden plate we use and his length/width dimension, above the shape we want to cut inside.
 		svg_builder.draw_rectangle(0.5,0.5,selectPlanche[indexSelection].width,selectPlanche[indexSelection].length,"svgLayer2");
-		svg_builder.draw_text(selectPlanche[indexSelection].thickness + 47,  selectPlanche[indexSelection].thickness + 15, BOX_SCHEME_WIDTH + " / " + selectPlanche[indexSelection].width, "svgLayer2");
+		svg_builder.draw_text(selectPlanche[indexSelection].thickness + 45,  selectPlanche[indexSelection].thickness - 15, BOX_SCHEME_WIDTH + " / " + selectPlanche[indexSelection].width, "svgLayer2");
 		svg_builder.draw_text(selectPlanche[indexSelection].thickness + 5, selectPlanche[indexSelection].thickness + 60, BOX_SCHEME_LENGTH + " / " + selectPlanche[indexSelection].length, "svgLayer2");
 	},
 	
 	/**
-	 *	function that set the values width, height and the viewbox of the svg tag, using the biggest value among the wooden plate (width/length) and
-	 *	the scheme you drawn (width/length). => it result in a good view of both of these items.
+	 *	function that set/reset the values width, height and the viewbox of the svg tag, using the biggest value among the wooden plate (width/length) and
+	 *	the scheme you drawn (width/length). => it result in a good scaling view of both of these items. 
 	 */
 	set_viewbox: function () {
 		if( BOX_SCHEME_WIDTH > selectPlanche[indexSelection].width ) {
@@ -135,8 +141,7 @@ var svg_builder = {
 	},
 	
 	/**
-	 *	function called by a checkbox, it display or hide the second layout for our svg.
-	 *	@param {boolean} show if true the layer 2 will be display, else it wont.
+	 *	function that display or hide the second layout for our svg.
 	 */
 	show_layer2: function() {
 		var layer = document.getElementById("svgLayer2");
@@ -335,6 +340,14 @@ var svg_builder = {
 
 	/**
 	 *	function that modify the direction of the drawing path using things like this scheme : "value1,value2"
+	 *	<p> 0 or (a value > 7) : draws the top side/path from left to right </p>
+	 *	<p> 1 : draw from left to right reversed == default reversed </p>
+	 *	<p> 2 : draw from right to left </p>
+	 *	<p> 3 : draw from right to left reversed </p>
+	 *	<p> 4 : draw from top to bottom </p>
+	 *	<p> 5 : draw from top to bottom reversed </p>
+	 *	<p> 6 : draw from bottom to top </p>
+	 *	<p> 7 : draw from bottom to top reversed </p>
 	 * 	@example it can switch "value1,value2" into "value2,value1"
 	 *	@param scheme {string} which is like "value1,value2"
 	 * 	@param rotate_case {int} is the case we want to use, there is 8 differents
@@ -603,21 +616,24 @@ function check_parameters_constraint(wooden_plate_width, wooden_plate_length, wo
  *	@param height_box {int} its the height of the box
  */
 function tests(wooden_plate_thickness, width_box, depth_box, height_box) {
-
+	
 	document.getElementById("previsualisation").click();
 	svg_builder.clear_svg("svgLayer1");
 	svg_builder.clear_svg("svgLayer2");
 	wooden_plate_width = selectPlanche[indexSelection].width;
 	wooden_plate_length = selectPlanche[indexSelection].length;
-	wooden_plate_thickness = selectPlanche[indexSelection].thickness; // = 5;
-	width_box = Number(document.getElementById("longueur").value); // = 200;
-	depth_box = Number(document.getElementById("largeur").value); // = 50;
-	height_box = Number(document.getElementById("hauteur").value); // = 50;
-	var notch_size = Number(document.getElementById("encoche").value); // = 10;
+	wooden_plate_thickness = selectPlanche[indexSelection].thickness; 	// = 5; 	// as an exemple.
+	width_box = Number(document.getElementById("longueur").value); 		// = 200;
+	depth_box = Number(document.getElementById("largeur").value); 		// = 50;
+	height_box = Number(document.getElementById("hauteur").value); 		// = 50;
+	var notch_size = Number(document.getElementById("encoche").value); 	// = 10;
 	
 	height_box = height_box - wooden_plate_thickness * 2; // to correct the height lack ( its the fact that we must count the wooden_plate_thickness ! )
-	if( !( notch_size >= 3 ) || !( notch_size < ( 0.40 * Math.min(depth_box, height_box)) ) ) { NOTCH_SIZE = NOTCH_SIZE_DEFAULT; } // to correct the value if the notch size given isnt good
-	else { NOTCH_SIZE = notch_size; }
+	
+	// how to handle the size of the notch parameter ?
+	/*if( !( notch_size >= 3 ) || !( notch_size < ( 0.40 * Math.min(depth_box, height_box)) ) ) { NOTCH_SIZE = NOTCH_SIZE_DEFAULT; } // to correct the value if the notch size given isnt good
+	else { NOTCH_SIZE = notch_size; }*/ 
+	NOTCH_SIZE = notch_size;
 	
 	var error_id = check_parameters_constraint(wooden_plate_width, wooden_plate_length, wooden_plate_thickness, width_box, depth_box, height_box, notch_size)
 		switch( error_id ) {
@@ -673,10 +689,6 @@ function tests(wooden_plate_thickness, width_box, depth_box, height_box) {
 		}
 	}
 	
-	svg_builder.show_layer2();
 	svg_builder.generate_svg_file();
-	
-	// on retire la viewBox pour que notre affichage sur le site reste visible avec des proportions correctes
-	var svg = document.getElementById("svg");
-	svg.removeAttribute("transform");
+	svg_builder.show_layer2();
 }
