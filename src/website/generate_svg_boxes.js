21615da8c7fc65c0a5e733beabddabc2a58157c2
Axel Murat <axel.murat@laposte.net>
2018-04-30 10:06:01 +0200

    éveloppement: Auto stash before merge of "développement" and "origin/développement"

--- src/website/generate_svg_boxes.js
+++ src/website/generate_svg_boxes.js
@@ -75,10 +75,8 @@ var svg_builder = {
 	 *	then we use the magic function click that simulate a human click on this a tag, which open the download yes/no window.
 	 */
 	generate_svg_file: function () {
-		svg_builder.define_attributes_box(BOX_SCHEME_WIDTH, BOX_SCHEME_LENGTH);
-		// hidding the wooden plate
-		var layer = document.getElementById("svgLayer2"); 
-		layer.setAttribute("opacity", "0"); 
+		// resetting the viewbox
+		svg_builder.set_viewbox();
 		// Use XMLSerializer to convert the DOM to a string
 		var s = new XMLSerializer();
 		var d = document.getElementById("svg");
@@ -91,10 +89,6 @@ var svg_builder = {
 		document.getElementById("filesvg").setAttribute("download", "thismustbethebest.svg");
 		// our a tag is hidden, so we use the click function as we would click on it usualy
 		document.getElementById("filesvg").click();
-		// show the wooden plate
-		layer.setAttribute("opacity", "1"); 
-		// on retire la viewBox pour que notre affichage sur le site reste visible avec des proportions correctes
-		d.removeAttribute("transform");
 	},
 
 	/** 
@@ -119,8 +113,8 @@ var svg_builder = {
 	},
 	
 	/**
-	 *	function that set/reset the values width, height and the viewbox of the svg tag, using the biggest value among the wooden plate (width/length) and
-	 *	the scheme you drawn (width/length). => it result in a good scaling view of both of these items. 
+	 *	function that set the values width, height and the viewbox of the svg tag, using the biggest value among the wooden plate (width/length) and
+	 *	the scheme you drawn (width/length). => it result in a good view of both of these items.
 	 */
 	set_viewbox: function () {
 		if( BOX_SCHEME_WIDTH > selectPlanche[indexSelection].width ) {
@@ -139,7 +133,8 @@ var svg_builder = {
 	},
 	
 	/**
-	 *	function that display or hide the second layout for our svg.
+	 *	function called by a checkbox, it display or hide the second layout for our svg.
+	 *	@param {boolean} show if true the layer 2 will be display, else it wont.
 	 */
 	show_layer2: function() {
 		var layer = document.getElementById("svgLayer2");
@@ -200,9 +195,9 @@ var svg_builder = {
 	 * @param {int} Bx the x (abscissa) position where we must move from Ax
 	 * @param {int} By the y (ordinate) position where we must move from Ay
 	 */
-	draw_line: function (Ax, Ay, Bx, By) {
+	draw_line: function (Ax, Ay, Bx, By,angle=0) {
 		var tab_coordinate = "m " + Ax + "," + Ay + " " + Bx + "," + By + " "; // just put the relative mod for svg path "m" and take start drawing at (draw_origin_x, draw_origin_y)
-		svg_builder.create_path(tab_coordinate);
+		svg_builder.create_path(tab_coordinate,angle);
 	},
 
 	/**
@@ -215,11 +210,11 @@ var svg_builder = {
 	 * @param draw_origin_x {int} is the x (abscissa) position where we start the drawing.
 	 * @param draw_origin_y {int} is the y (ordinate) position where we start the drawing.
 	 */
-	draw_path: function (wooden_plate_thickness, size, rotate_case, draw_origin_x, draw_origin_y) {
+	draw_path: function (wooden_plate_thickness, size, rotate_case, draw_origin_x, draw_origin_y,angle=0,nbNotch=0) {
 		var tab_coordinate = svg_builder.draw_side(wooden_plate_thickness, size); 							// gets the good values to draw
 		tab_coordinate = svg_builder.rotate_path(tab_coordinate, rotate_case) 								// rotate them if need be
 		tab_coordinate = "m " + draw_origin_x + "," + draw_origin_y + " " + tab_coordinate; // just put the relative mod for svg path "m" and take start drawing at (draw_origin_x, draw_origin_y)
-		svg_builder.create_path(tab_coordinate);
+		svg_builder.create_path(tab_coordinate,angle);
 	},
 
 	/**
@@ -231,13 +226,13 @@ var svg_builder = {
 	 * @param draw_origin_x {int} is the x (abscissa) position where we start the drawing.
 	 * @param draw_origin_y {int} is the y (ordinate) position where we start the drawing.
 	 */
-	draw_path_right_left_correction: function (wooden_plate_thickness, size, rotate_case, draw_origin_x, draw_origin_y) {
+	draw_path_right_left_correction: function (wooden_plate_thickness, size, rotate_case, draw_origin_x, draw_origin_y,angle=0) {
 		var tab_coordinate = svg_builder.draw_side(wooden_plate_thickness, size); 							// gets the good values to draw
 		tab_coordinate = svg_builder.rotate_path(tab_coordinate, rotate_case) 								// rotate them if need be
 		tab_coordinate = "m " + draw_origin_x + "," + draw_origin_y + " " + tab_coordinate; // just put the relative mod for svg path "m" and take start drawing at (draw_origin_x, draw_origin_y)
 		tab_coordinate = tab_coordinate.split(" ");
 		tab_coordinate[3] = ""; tab_coordinate[tab_coordinate.length - 2] = "";
-		svg_builder.create_path(tab_coordinate.join(' '));
+		svg_builder.create_path(tab_coordinate.join(' '),angle);
 	},
 	
 	/** 
@@ -245,7 +240,7 @@ var svg_builder = {
 	 *	@param {string[]} tab_coordinate the values (x,y) of the different path position 
 	 *  @param {integer} [angle=0] - Angle of the path (optional parameter, default value = 0)
 	 */
-	create_path: function (tab_coordinate, angle=0) {
+	create_path: function (tab_coordinate, angle) {
 		var svg = document.getElementById("svgLayer1");
 		var newpath = document.createElementNS(svg.namespaceURI,"path");  
 		newpath.setAttribute("d", tab_coordinate);
@@ -260,11 +255,27 @@ var svg_builder = {
 	 * 	@param size {int} is the length for which we have to make a path
 	 *	@return {string} containing the values (x,y) of the different path position for an entire side
 	 */
-	draw_side: function (wooden_plate_thickness, size) {
+	draw_side: function (wooden_plate_thickness, size,nbNotch=0) {
 		var inner_size = size - ( wooden_plate_thickness * 2 );
 		var coordinate_inner_side = svg_builder.draw_inner_side(wooden_plate_thickness, inner_size);
 		return "0,0 " + wooden_plate_thickness + ",0 " + coordinate_inner_side + wooden_plate_thickness + ",0";
 	},
+
+
+
+
+	draw_inner_side2: function(){
+		var number_notch = 3;
+		var size_rest = ( size - ( number_notch * NOTCH_SIZE));
+		var size_rest_parts = ( size_rest / ( number_notch + 1 ) );
+		var tab_coordinate = [];
+		for( var i = 0 ; i < number_notch ; i++ ) {
+			svg_builder.next_coordinate_non_notch(tab_coordinate,size_rest_parts);
+			svg_builder.next_coordinate_notch(tab_coordinate,wooden_plate_thickness);
+		}
+		svg_builder.next_coordinate_non_notch(tab_coordinate,size_rest_parts);
+		return tab_coordinate.join('');
+	}
 
 	/**
 	 *	function that return a string with all the scheme "value1,value2" as "x,y" which represent the inner part of a side
@@ -272,8 +283,14 @@ var svg_builder = {
 	 * 	@param size {int} is the length for which we have to make a path
 	 *	@return {string} containing the values (x,y) of the different path position for the inner part of a side
 	 */
-	draw_inner_side: function (wooden_plate_thickness, size) {
-		var number_notch = svg_builder.get_number_notch(wooden_plate_thickness, size);
+	draw_inner_side: function (wooden_plate_thickness, size, nbNotch=0) {
+
+		if(nbNotch = 0){
+			var number_notch = svg_builder.get_number_notch(wooden_plate_thickness, size);
+		}
+		else{
+			var number_notch = nbNotch;
+		}
 		var size_rest = ( size - ( number_notch * NOTCH_SIZE ) );
 		var size_rest_parts = ( size_rest / ( number_notch + 1 ) );
 		var tab_coordinate = [];
@@ -338,14 +355,6 @@ var svg_builder = {
 
 	/**
 	 *	function that modify the direction of the drawing path using things like this scheme : "value1,value2"
-	 *	<p> 0 or (a value > 7) : draws the top side/path from left to right </p>
-	 *	<p> 1 : draw from left to right reversed == default reversed </p>
-	 *	<p> 2 : draw from right to left </p>
-	 *	<p> 3 : draw from right to left reversed </p>
-	 *	<p> 4 : draw from top to bottom </p>
-	 *	<p> 5 : draw from top to bottom reversed </p>
-	 *	<p> 6 : draw from bottom to top </p>
-	 *	<p> 7 : draw from bottom to top reversed </p>
 	 * 	@example it can switch "value1,value2" into "value2,value1"
 	 *	@param scheme {string} which is like "value1,value2"
 	 * 	@param rotate_case {int} is the case we want to use, there is 8 differents
@@ -591,6 +600,46 @@ var Box_without_top = {
 	}
 };
 
+var Toolbox = {
+
+	draw_single_part: function (number_part, origin_x, origin_y, wooden_plate_thickness, width_box, depth_box, height_box, bool_top, bool_right, bool_bot, bool_left) {
+		if(number_part == 1) {
+			if(bool_top)	svg_builder.draw_line(origin_x, origin_y, width_box, 0);
+			if(bool_right) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 4, origin_x + width_box, origin_y);
+			if(bool_bot) 	svg_builder.draw_path(wooden_plate_thickness, width_box, 3, origin_x + width_box, origin_y + height_box);
+			if(bool_left) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 6, origin_x, origin_y + height_box);
+			svg_builder.define_box_width_and_length(width_box + 10, height_box + 10);
+		} else if(number_part == 2) {
+			if(bool_top) 	svg_builder.draw_path(wooden_plate_thickness, width_box, 0, origin_x, origin_y);
+			if(bool_right) 	svg_builder.draw_path(wooden_plate_thickness, depth_box, 4, origin_x + width_box, origin_y);
+			if(bool_bot) 	svg_builder.draw_path(wooden_plate_thickness, width_box, 2, origin_x + width_box, origin_y + depth_box);
+			if(bool_left) 	svg_builder.draw_path(wooden_plate_thickness, depth_box, 6, origin_x, origin_y + depth_box);
+			svg_builder.define_box_width_and_length(width_box + 10, depth_box + 10);
+		} else if(number_part == 3) {
+			if(bool_top) 	svg_builder.draw_path(wooden_plate_thickness, width_box, 1, origin_x, origin_y );
+			if(bool_right) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 4, origin_x + width_box, origin_y);
+			if(bool_bot) 	svg_builder.draw_line(origin_x + width_box, origin_y + height_box, -width_box, 0);
+			if(bool_left) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 6, origin_x, origin_y + height_box);
+			svg_builder.define_box_width_and_length(width_box + 10, height_box + 10);
+		} else if(number_part == 4) {
+			if(bool_top) 	svg_builder.draw_line(origin_x, origin_y, depth_box - wooden_plate_thickness * 2, 0);
+			if(bool_right)	svg_builder.draw_path(wooden_plate_thickness, height_box, 5, origin_x + depth_box - wooden_plate_thickness * 2, origin_y);
+			if(bool_bot) 	svg_builder.draw_path_right_left_correction(wooden_plate_thickness, depth_box, 3, origin_x + depth_box - wooden_plate_thickness * 2, origin_y + height_box);
+			if(bool_left) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 7, origin_x, origin_y + height_box);
+			svg_builder.define_box_width_and_length(depth_box + 10, height_box + 10);
+		} else if(number_part == 5) {
+			if(bool_top)	svg_builder.draw_path_right_left_correction(wooden_plate_thickness, depth_box, 1, origin_x, origin_y);
+			if(bool_right) 	svg_builder.draw_path(wooden_plate_thickness, height_box, 5, origin_x  + depth_box - wooden_plate_thickness * 2, origin_y);
+			if(bool_bot) 	svg_builder.draw_line(origin_x + depth_box - wooden_plate_thickness * 2, origin_y + height_box, - depth_box + wooden_plate_thickness * 2, 0);
+			if(bool_left)	svg_builder.draw_path(wooden_plate_thickness, height_box, 7, origin_x, origin_y + height_box);
+			svg_builder.define_box_width_and_length(depth_box + 10, height_box + 10);
+		}
+
+	}
+
+
+};
+
 /**
  * function that check is the constraint of our initial parameters are respected or not, return a value depending on the error, 0 is no error.
 	 *	@param wooden_plate_width {int} its the width of the wooden plate
@@ -614,19 +663,21 @@ function check_parameters_constraint(wooden_plate_width, wooden_plate_length, wo
  *	@param height_box {int} its the height of the box
  */
 function tests(wooden_plate_thickness, width_box, depth_box, height_box) {
-	
+
 	document.getElementById("previsualisation").click();
 	svg_builder.clear_svg("svgLayer1");
 	svg_builder.clear_svg("svgLayer2");
 	wooden_plate_width = selectPlanche[indexSelection].width;
 	wooden_plate_length = selectPlanche[indexSelection].length;
-	wooden_plate_thickness = selectPlanche[indexSelection].thickness; 	// = 5; 	// as an exemple.
-	width_box = Number(document.getElementById("longueur").value); 		// = 200;
-	depth_box = Number(document.getElementById("largeur").value); 		// = 50;
-	height_box = Number(document.getElementById("hauteur").value); 		// = 50;
-	var notch_size = Number(document.getElementById("encoche").value); 	// = 10;
+	wooden_plate_thickness = selectPlanche[indexSelection].thickness; // = 5;
+	width_box = Number(document.getElementById("longueur").value); // = 200;
+	depth_box = Number(document.getElementById("largeur").value); // = 50;
+	height_box = Number(document.getElementById("hauteur").value); // = 50;
+	var notch_size = Number(document.getElementById("encoche").value); // = 10;
 	
 	height_box = height_box - wooden_plate_thickness * 2; // to correct the height lack ( its the fact that we must count the wooden_plate_thickness ! )
+	if( !( notch_size >= 3 ) || !( notch_size < ( 0.40 * Math.min(depth_box, height_box)) ) ) { NOTCH_SIZE = NOTCH_SIZE_DEFAULT; } // to correct the value if the notch size given isnt good
+	else { NOTCH_SIZE = notch_size; }
 	
 	// how to handle the size of the notch parameter ?
 	/*if( !( notch_size >= 3 ) || !( notch_size < ( 0.40 * Math.min(depth_box, height_box)) ) ) { NOTCH_SIZE = NOTCH_SIZE_DEFAULT; } // to correct the value if the notch size given isnt good
@@ -687,6 +738,10 @@ function tests(wooden_plate_thickness, width_box, depth_box, height_box) {
 		}
 	}
 	
-	svg_builder.generate_svg_file();
 	svg_builder.show_layer2();
+	svg_builder.generate_svg_file();
+	
+	// on retire la viewBox pour que notre affichage sur le site reste visible avec des proportions correctes
+	var svg = document.getElementById("svg");
+	svg.removeAttribute("transform");
 }
